5e679e0cd77a950ca9dd1984137462d4980b0c6d
chatid <379424619@qq.com>
2021-12-08 00:57:45 +0800

    springboot

---
+++ src/main/java/com/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/test")
+public class TestController {
+
+    @ResponseBody
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+}
